617e6670f4de88517ba5fc46632759e7ddd2fba2
Paul Lebedev <pavell@realconnex.com>
2018-10-30 16:53:27 +0300

    Services domain names

---
+++ src/HttpServices.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Realconnex;
+
+class HttpServices
+{
+    public const GREEN='green';
+    public const BLUE='blue';
+    public const MC='mc';
+    public const FEED='feed';
+    public const MBAU='mbau';
+    public const SEARCH='search';
+    public const EMAIL='email';
+    public const FAPI='fapi';
+    public const FILE='file';
+}
