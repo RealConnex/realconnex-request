c7b671730eeca1a22328571a97b99586af58af1d
Paul Lebedev <pavell@realconnex.com>
2018-11-08 12:08:16 +0300

    Auth tests

---
+++ src/Auth.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Realconnex;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Auth
+{
+    const HEADER_AUTH_TOKEN = 'Authorization';
+
+    /** @var string */
+    private $token;
+
+    /** @var bool */
+    private $authorize = false;
+
+    public function __construct(RequestStack $request)
+    {
+        $this->token = $request->getCurrentRequest()->headers->get(self::HEADER_AUTH_TOKEN) ?? '';
+    }
+
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    public function authorize(): bool
+    {
+        return $this->authorize;
+    }
+
+    public function setToken(string $token): void
+    {
+        $this->token = $token;
+    }
+
+    public function setAuthorize(bool $authorize): void
+    {
+        $this->authorize = $authorize;
+    }
+}
